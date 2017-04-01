6af3976ea8713831d1349a243fba327a5149e8ed
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-04-01 10:23:40 +0200

    ptpb.pw service menu added

---
+++ os-install/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function ajustarReloj(){
+	timedatectl set-ntp true
+}
+
+function particionarDisco(){
+	:
+}
+
+ajustarReloj
+particionarDisco
---
+++ plasma/ptpb.pw/bin/ptpb.pw.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -sF "c=@${1:--}" -w "%{redirect_url}" 'https://ptpb.pw/?r=1' -o /dev/stderr | xsel -l /dev/null -b
