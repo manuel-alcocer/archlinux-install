1d71a8ab5b8f4cac5f5d8b22cca951bd07201818
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-04-01 11:52:02 +0200

    despistes...

--- plasma/ptpb.pw/install.sh
+++ plasma/ptpb.pw/install.sh
@@ -2,17 +2,26 @@
 
 function crearRutas(){
     serviceMenusPath="$HOME/.local/share/kservices5/ServiceMenus"
-    [[ ! -d $serviceMenusPath ]] && mkdir -p "${serviceMenusPath}"
-    [[ $? != 0 ]] && { printf "Hubieron errores creando la ruta: ${serviceMenusPath}...\nSaliendo..\n"; exit 1 }
+    [[ ! -d "$serviceMenusPath" ]] && {
+        mkdir -p "${serviceMenusPath}"
+        [[ $? != 0 ]] && {
+            printf "Hubieron errores creando la ruta: ${serviceMenusPath}...\nSaliendo..\n";
+            exit 1;
+        };
+    }
     scriptPath="$HOME/.local/bin"
-    [[ ! -d $scriptPath ]] && { printf "Hubieron errores creando la ruta: ${serviceMenusPath}...\nSaliendo..\n"; exit 1 }
+    [[ ! -d "$scriptPath" ]] && { 
+        mkdir -p "${scriptPath}"
+        [[ $? != 0 ]] && {
+            printf "Hubieron errores creando la ruta: ${scriptPath}...\nSaliendo..\n";
+        exit 1;
+        };
+    }
 }
 
 function descargarFicheros(){
-    curl -o "${serviceMenusPath}/sendtoptpb.desktop" \
-        'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/ServiceMenus/sendptpb.desktop'
-    curl -o "${scriptPath}/ptpb.pw.sh" \
-        'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/bin/ptpb.pw.sh'
+    curl -o "${serviceMenusPath}/sendtoptpb.desktop" 'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/ServiceMenus/sendptpb.desktop'
+    curl -o "${scriptPath}/ptpb.pw.sh" 'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/bin/ptpb.pw.sh'
 }
 
 crearRutas
