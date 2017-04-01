a2e2d69d1d77787d39e0da2ecf2abe3f4a06af57
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-04-01 12:00:38 +0200

    ajuste de permisos y usuario

--- plasma/ptpb.pw/install.sh
+++ plasma/ptpb.pw/install.sh
@@ -24,5 +24,21 @@ function descargarFicheros(){
     curl -o "${scriptPath}/ptpb.pw.sh" 'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/bin/ptpb.pw.sh'
 }
 
+
+function configurarFicheros(){
+    chmod u+x "${scriptPath}/ptpb.pw.sh"
+    [[ $? != 0 ]] && {
+        printf 'Hubieron errores dando permisos de ejeción al ejecutable...\n';
+        printf 'La instalación no está completa\n';
+    }
+    sed -i "s/usuario/$USER/" "${serviceMenusPath}/sendptpb.desktop"
+    [[ $? != 0 ]] && {
+        printf 'Hubieron errores cambiando la ruta del ejecutable...\n';
+        printf 'La instalación no está completa\n';
+    }
+}
+
 crearRutas
 descargarFicheros
+configurarFicheros
+
