66f26c70c56cd776218a2bd75b0657c66764fb88
Manuel Alcocer Jiménez <manuel@alcocer.net>
2017-04-01 11:39:42 +0200

    installer finished

--- plasma/ptpb.pw/install.sh
+++ plasma/ptpb.pw/install.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
+function crearRutas(){
+    serviceMenusPath="$HOME/.local/share/kservices5/ServiceMenus"
+    [[ ! -d $serviceMenusPath ]] && mkdir -p "${serviceMenusPath}"
+    [[ $? != 0 ]] && { printf "Hubieron errores creando la ruta: ${serviceMenusPath}...\nSaliendo..\n"; exit 1 }
+    scriptPath="$HOME/.local/bin"
+    [[ ! -d $scriptPath ]] && { printf "Hubieron errores creando la ruta: ${serviceMenusPath}...\nSaliendo..\n"; exit 1 }
+}
 
+function descargarFicheros(){
+    curl -o "${serviceMenusPath}/sendtoptpb.desktop" \
+        'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/ServiceMenus/sendptpb.desktop'
+    curl -o "${scriptPath}/ptpb.pw.sh" \
+        'https://raw.githubusercontent.com/manuel-alcocer/archlinux-install/master/plasma/ptpb.pw/bin/ptpb.pw.sh'
+}
 
+crearRutas
+descargarFicheros
